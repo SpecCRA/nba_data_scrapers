df29cc7b80e7e18f695a7fdecc1a000426c9aa91
Ben Xiao <benjamin.y.xiao@gmail.com>
2018-02-07 00:30:32 -0800

    chore: deleted test code lines from script files

--- bref_player_stats_scraper.py
+++ bref_player_stats_scraper.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[83]:
+# In[1]:
 
 
 from bs4 import BeautifulSoup
@@ -12,7 +12,7 @@ import time
 import sys
 
 
-# In[84]:
+# In[2]:
 
 
 # Create url templates for each kind of stats
@@ -27,7 +27,7 @@ url_template_list = [per_g_url_template, adv_url_template, tot_url_template,
                      per_36m_url_template,]
 
 
-# In[85]:
+# In[3]:
 
 
 # Ask user to input start and end years
@@ -43,7 +43,7 @@ except:
     print('Enter a valid 4 digit year.')
 
 
-# In[86]:
+# In[4]:
 
 
 # Check if end year is after start year
@@ -65,7 +65,7 @@ check_year(user_start_year)
 check_year(user_end_year)
 
 
-# In[87]:
+# In[5]:
 
 
 # Create empty lists to store data before appending to Dataframe
@@ -75,7 +75,7 @@ player_data = []
 df = pd.DataFrame()
 
 
-# In[88]:
+# In[6]:
 
 
 # Empty DataFrames for each set of pages
@@ -89,7 +89,7 @@ df_per_36m = pd.DataFrame()
 df_list = [df_per_g, df_adv, df_tot, df_per_36m]
 
 
-# In[89]:
+# In[7]:
 
 
 # Get column headers from each page
@@ -105,29 +105,7 @@ def get_column_headers(soup):
     #column_headers = [th.getText() for th in soup.find('tr').findAll('th')]
 
 
-# In[90]:
-
-
-# old function that's a mess
-def get_player_data(soup):
-    temp_player_data = []
-    for i in range(len(soup.findAll('tr')[1:])):
-        # temp list to store player data
-        player_row = []
-        
-        # Loop through 'td' tags to extract player data
-        for td in soup.findAll('tr')[1:][i].findAll('td'):
-            player_row.append(td.getText())
-        
-        # Append data to a list    
-        temp_player_data.append(player_row)
-        
-        # Replace global variable with gathered player data
-    print(temp_player_data)
-    player_data = temp_player_data
-
-
-# In[91]:
+# In[8]:
 
 
 # Function to get player data from each page
@@ -144,9 +122,10 @@ def get_player_data(soup):
     return temp_player_data
 
 
-# In[92]:
+# In[9]:
 
 
+# This function scrapes the stats data of one page and returns it in a DataFrame
 def scrape_page(url):
     r = requests.get(url) # get the url
     soup = BeautifulSoup(r.text, 'html.parser') # Create BS object
@@ -164,7 +143,7 @@ def scrape_page(url):
     return df
 
 
-# In[93]:
+# In[10]:
 
 
 def get_season(input_year):
@@ -173,7 +152,7 @@ def get_season(input_year):
     return season
 
 
-# In[94]:
+# In[11]:
 
 
 # This function drops empty rows an columns, drops duplicates, and changes
@@ -198,7 +177,7 @@ def gen_cleaning(df):
     return df
 
 
-# In[95]:
+# In[ ]:
 
 
 # This function scrapes player data from multiple pages by start and end years
@@ -234,21 +213,7 @@ def scrape_pages(url_template, start_year, end_year, output_df):
     return output_df
 
 
-# In[96]:
-
-
-# This bunch of code is just for me to check things as I go
-
-#url = "https://www.basketball-reference.com/leagues/NBA_2006_per_game.html"
-#r = requests.get(url)
-#soup = BeautifulSoup(r.text, 'html.parser')
-#column_headers = get_column_headers(soup)
-#player_data = get_player_data(soup)
-#df_test = pd.DataFrame(player_data, columns = column_headers[1:])
-#df_test = gen_cleaning(df_test)
-
-
-# In[97]:
+# In[ ]:
 
 
 # Fill each DataFrame with data scraped from their respective pages
@@ -270,7 +235,7 @@ end = time.time()
 print("Time elapsed :" +str((end - start) / 60) + " minutes")
 
 
-# In[98]:
+# In[ ]:
 
 
 # Check all column names to see what needs to be cleaned
@@ -285,7 +250,7 @@ print("advanced")
 print(list(df_adv))
 
 
-# In[99]:
+# In[ ]:
 
 
 # Label columns properly by adding "_tot" to the end of some column values
@@ -294,14 +259,14 @@ df_tot.columns.values[[7, 8 , 9, 11, 12, 14, 15, 18, 19]] = [df_tot.columns.valu
 df_tot.columns.values[21:30] = [df_tot.columns.values[21:30][col] + "_tot" for col in range(9)]
 
 
-# In[100]:
+# In[ ]:
 
 
 # Check column titles again
 list(df_tot)
 
 
-# In[101]:
+# In[ ]:
 
 
 # drop _perc columns from per_g and per_36m
@@ -316,20 +281,20 @@ df_per_g.columns.values[21:29] = [df_per_g.columns.values[21:30][col] + "_per_G"
 df_per_g.rename(columns={'PS/G': 'PTS_per_G'}, inplace = True)
 
 
-# In[102]:
+# In[ ]:
 
 
 df_per_36m.columns.values[[7, 8, 9, 11, 12, 14, 15, 18, 19]]
 
 
-# In[103]:
+# In[ ]:
 
 
 # Check if proper values were changed
 list(df_per_g)
 
 
-# In[104]:
+# In[ ]:
 
 
 df_per_36m.columns.values[[8, 9, 11, 12, 14, 15, 17, 18]] = [df_per_36m.columns.values[[8, 9, 11, 12, 14, 15, 17, 18]][col] + "_per_36m" for col in range(8)]
@@ -337,14 +302,14 @@ df_per_36m.columns.values[[8, 9, 11, 12, 14, 15, 17, 18]] = [df_per_36m.columns.
 df_per_36m.columns.values[20:30] = [df_per_36m.columns.values[20:30][col] + "_per_36m"                                    for col in range(9)]
 
 
-# In[105]:
+# In[ ]:
 
 
 # Check columns were changed properly
 list(df_per_36m)
 
 
-# In[106]:
+# In[ ]:
 
 
 # Find where '\xa0' columns are for removal
@@ -352,7 +317,7 @@ print(df_adv.columns[-5])
 print(df_adv.columns[19])
 
 
-# In[107]:
+# In[ ]:
 
 
 # Drop '\xa0' columns, last one first
@@ -360,26 +325,26 @@ print(df_adv.columns[19])
 #df_adv.drop(df_adv.columns[19], axis = 1, inplace = True)
 
 
-# In[108]:
+# In[ ]:
 
 
 list(df_adv)
 
 
-# In[109]:
+# In[ ]:
 
 
 df_adv.rename(columns = {'WS/48' : 'WS_per_48'}, inplace = True)
 
 
-# In[110]:
+# In[ ]:
 
 
 # Check to see if columns were dropped properly
 list(df_adv)
 
 
-# In[111]:
+# In[ ]:
 
 
 # Merge dataframes later on season, player name, and team
@@ -388,7 +353,7 @@ list(df_adv)
 # Common things: Season, Player, Pos, Age, Tm, G
 
 
-# In[112]:
+# In[ ]:
 
 
 df_all = pd.merge(df_tot, df_per_g, how = "left", 
@@ -396,7 +361,7 @@ df_all = pd.merge(df_tot, df_per_g, how = "left",
                       '3P_perc', '2P_perc', 'FG_perc', 'eFG_perc'])
 
 
-# In[113]:
+# In[ ]:
 
 
 df_all = pd.merge(df_all, df_per_36m, how = "left",
@@ -404,55 +369,55 @@ df_all = pd.merge(df_all, df_per_36m, how = "left",
                       '3P_perc', '2P_perc', 'FG_perc'])
 
 
-# In[114]:
+# In[ ]:
 
 
 df_all = pd.merge(df_all, df_adv, how = "left",
                 on = ['Season', 'Player', 'Pos', 'Age', 'Tm', 'G'])
 
 
-# In[115]:
+# In[ ]:
 
 
 # Check columns to make sure they're all right
 list(df_all)
 
 
-# In[116]:
+# In[ ]:
 
 
 # Try to drop duplicate MP columns
 list(df_all.drop(['MP_x', 'MP_y'], axis = 1))
 
 
-# In[117]:
+# In[ ]:
 
 
 df_all.drop(['MP_x', 'MP_y'], axis = 1, inplace = True)
 
 
-# In[118]:
+# In[ ]:
 
 
 # Final check of columns
 list(df_all)
 
 
-# In[119]:
+# In[ ]:
 
 
 # First check length of dataframe
-#print(len(df_all))
+print(len(df_all))
 
 
-# In[120]:
+# In[ ]:
 
 
 # Fill Null values with 0
 df_all.fillna(0, inplace = True)
 
 
-# In[121]:
+# In[ ]:
 
 
 # Address ambiguous positions and combination positions
@@ -460,32 +425,32 @@ df = df_all.groupby(['Pos'])['Pos'].nunique()
 df
 
 
-# In[122]:
+# In[ ]:
 
 
 # Remove where 'Pos' value is 0
 df_all = df_all[df_all['Pos'] != 0]
 
 # Then check df_all length again
-#print(len(df_all))
+print(len(df_all))
 
 
-# In[123]:
+# In[ ]:
 
 
 # I think the PG-SF and C-SF positions are mistakes
 # Check the value to see the player
-#df_all[df_all['Pos'] == 'C-SF']
+df_all[df_all['Pos'] == 'C-SF']
 
 
-# In[124]:
+# In[ ]:
 
 
 # Check Bobby Jones' actual, commonly played position
-#df_all[df_all['Player'] == 'Bobby Jones']
+df_all[df_all['Player'] == 'Bobby Jones']
 
 
-# In[125]:
+# In[ ]:
 
 
 # Create list of dual positions in DataFrame
@@ -495,7 +460,7 @@ dual_pos_rows = []
 df_dual_pos = pd.DataFrame(columns = column_names)
 
 
-# In[126]:
+# In[ ]:
 
 
 # Gather all the dual positions by seeing which ones have a dash
@@ -505,7 +470,7 @@ for pos in df_all['Pos']:
             dual_pos_rows.append(pos)
 
 
-# In[127]:
+# In[ ]:
 
 
 # Append all dual position rows to a new DataFrame for auditing
@@ -514,7 +479,7 @@ for pos in dual_pos_rows:
                                     ignore_index = True)
 
 
-# In[128]:
+# In[ ]:
 
 
 df_dual_pos
@@ -522,18 +487,35 @@ df_dual_pos
 # Certain players have multiple positions or changed positions
 
 
-# In[140]:
+# In[ ]:
 
 
-# Create a smaller, shuffled sample DataFrame to test cleaning function
-#df_test = df_all.sample(frac = 0.05).copy()
-#print(len(df_test))
+df_dual_pos.groupby(['Player']).size().reset_index(name = 'Count').sort_values(['Count'], ascending = False).head(n=10)
 
 
-# In[142]:
+# In[ ]:
 
 
-#most_common_pos_test = {}
+# Check what is going on with some players with multiple positions
+df_all[df_all['Player'] == 'Allen Iverson*']
+
+
+# In[ ]:
+
+
+# Find most common position for this player
+df_all[df_all['Player'] == 'Allen Iverson*'].groupby(['Pos']).size().reset_index(name = 'Count').sort_values(['Count'], ascending = False).iloc[0][0]
+
+
+# In[ ]:
+
+
+# Count of seasons played at most common position
+df_all[df_all['Player'] == 'Allen Iverson*'].groupby(['Pos']).size().iloc[0]
+
+
+# In[ ]:
+
 
 # Use dictionary as key to replace 'Pos' values in the big DataFrame
 most_common_pos = {}
@@ -544,16 +526,12 @@ pos_key = {'PG': '1', 'SG': '2', 'SF': '3', 'PF': '4', 'C': '5'}
 
 # Side note: This makes Tim Duncan a center
 
-
-# In[143]:
-
-
-# Goes through DataFrame, takes most common position played, saves it to a dictionary
 def grab_most_common_pos(df, pos_dict):
-    for index, row in df[['Player', 'Pos']].iterrows(): # Go through each row in the DataFrame
+    # Loop through a dataframe and assign names and most common positions to a dictionary
+    for index, row in df[['Player', 'Pos']].iterrows():
         player_name = row.iloc[0] # Assign player name to variable
         # subset position dataframe to a player
-        pos_df = df[df['Player'] == player_name].groupby('Pos').size()        .reset_index(name = 'Count')        .sort_values(['Count'], ascending = False) # Takes most commonly played position
+        pos_df = df[df['Player'] == player_name].groupby('Pos').size()        .reset_index(name = 'Count')        .sort_values(['Count'], ascending = False) 
         
         #dual_pos_dict = {} # Store dict of dual positions
         
@@ -577,11 +555,6 @@ def grab_most_common_pos(df, pos_dict):
     
     #return pos_dict
 
-
-# In[144]:
-
-
-# Takes most common positions, cleans dual positions, and assigns a rounded position 1-5
 def clean_pos(df, pos_dict):
     # Loop through rows to check players' positions
     grab_most_common_pos(df, pos_dict)
@@ -602,41 +575,25 @@ def clean_pos(df, pos_dict):
     return df
 
 
-# In[157]:
+# In[ ]:
 
 
-# Assigns rounded position to player in the DataFrame into a new column
-# This works for df_test, but not for df_all
+# This function takes in a DataFrame and adds a new column with Rounded Position values
 def assign_pos(df, pos_dict):    
     # Add a Rounded_Pos column and fill it from pos_dict
     df['Rounded_Pos'] = ''
     
-    for name, pos in pos_dict.items(): # Loops through names in dictionary
-        # This finds each player and assigns the rounded position to the DataFrame
+    for name, pos in pos_dict.items():
         df.Rounded_Pos[df['Player'] == name] = pos
-        #print(name, pos)
-    return df
 
 
-# In[146]:
+# In[ ]:
 
 
 clean_pos(df_all, most_common_pos)
 
 
-# In[147]:
-
-
-#clean_pos(df_test, most_common_pos_test)
-
-
-# In[149]:
-
-
-#assign_pos(df_test, most_common_pos_test)
-
-
-# In[158]:
+# In[ ]:
 
 
 assign_pos(df_all, most_common_pos)
@@ -652,12 +609,12 @@ assign_pos(df_all, most_common_pos)
 #df_top_50_scorers = df_all.sort_values('PTS_per_G', ascending = False).head(n=50)
 
 
-# In[161]:
+# In[ ]:
 
 
 # Write to CSV files and DONE!
 file_name = 'player_data_' + str(user_start_year) + '-' + str(user_end_year) + '.csv'
-print(file_name)
+#print(file_name)
 df_all.to_csv(file_name, encoding = 'utf-8', index = False)
 
 
